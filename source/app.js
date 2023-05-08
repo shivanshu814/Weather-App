e807286b827c26f9008a903c3c0dbb20687fce97
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-05-08 11:44:08 +0530

    Completed

---
+++ source/app.js
@@ -0,0 +1,61 @@
+/** @format */
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const hbs = require('hbs');
+
+require('./db/conn');
+const weatherContact = require('./models/weatherContact');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+const template_path = path.join(__dirname, '../templates/views');
+const partials_path = path.join(__dirname, '../templates/partials');
+
+// public static  path
+const stat_path = path.join(__dirname, '../public');
+app.use(express.static(stat_path));
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
+// routing
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+app.get('/contact', (req, res) => {
+	res.render('contact');
+});
+app.get('/weather', (req, res) => {
+	res.render('weather');
+});
+app.get('*', (req, res) => {
+	res.render('404error', {
+		errorMsg: 'Opps! Page Not Found ',
+	});
+});
+
+//data creating
+app.post('/contact', async (req, res) => {
+	try {
+		const contact = new weatherContact({
+			name: req.body.name,
+			email: req.body.email,
+			subject: req.body.subject,
+			msg: req.body.msg,
+		});
+		const contactForm = await contact.save();
+		res.status(201).render('index');
+	} catch (er) {
+		res.status(400).render('contact');
+	}
+});
+
+app.listen(port, () => {
+	console.log(`listening to the port ${port}`);
+});
